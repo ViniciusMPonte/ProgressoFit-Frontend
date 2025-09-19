05ded24c0b5de09bdd16eef000513cc4b4a01d82
vinicius <vinicius.mponte@gmail.com>
2025-09-19 20:53:06 -0300

    feat: instalação do fontawesome via cdn

--- src/view/BaseView.js
+++ src/view/BaseView.js
@@ -4,10 +4,22 @@ import { Routes } from "../router/Routes.js";
 
 export default class BaseView {
 
-    constructor(){
+    constructor() {
+        this.ensureFontAwesome();
         NavbarComponent.init({
             links: new Routes().routes
         })
+    }
+    
+    ensureFontAwesome() {
+        const linkId = "fontawesome-cdn";
+        if (document.getElementById(linkId)) return
+
+        const link = document.createElement("link");
+        link.id = linkId;
+        link.rel = "stylesheet";
+        link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css";
+        document.head.appendChild(link);
     }
 
     alert(message, type, container, timeout) {
