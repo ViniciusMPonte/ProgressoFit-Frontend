8ff9235b6e8328df4d6841c6d858c1957f34fb37
vinicius <vinicius.mponte@gmail.com>
2025-09-22 10:55:08 -0300

    fix: alteração método que pega todos os nomes das images avatares para evitar erro no terminal e deixar sincrono

--- src/view/component/AvatarComponent.js
+++ src/view/component/AvatarComponent.js
@@ -1,39 +1,24 @@
 export class AvatarComponent {
     constructor() {
-        this.avatarNames = [];
-        this.loadAllAvatarNames();
-    }
-
-    async loadAllAvatarNames() {
-        let imageIndex = 1;
-        
-        while (true) {
-            try {
-                const imageName = `avatar-${imageIndex}`;
-                const imageExists = await this.checkImageExists(`/src/assets/images/avatars/${imageName}.png`);
-                
-                if (!imageExists) {
-                    break;
-                }
-                
-                this.avatarNames.push(imageName);
-                imageIndex++;
-            } catch (error) {
-                break;
-            }
-        }
-    }
-
-    checkImageExists(src) {
-        return new Promise((resolve) => {
-            const img = new Image();
-            img.onload = () => resolve(true);
-            img.onerror = () => resolve(false);
-            img.src = src;
-        });
+        this.avatarNames = [
+            'avatar-1',
+            'avatar-2',
+            'avatar-3',
+            'avatar-4',
+            'avatar-5',
+            'avatar-6',
+        ];
     }
 
     getAvatarNames() {
         return this.avatarNames;
+    }
+
+    getAllAvatarImg() {
+        let buffer = ''
+        this.avatarNames.forEach((avatarName)=>{
+            buffer += `<img src="/src/assets/images/avatars/${avatarName}.png"/>`
+        })
+        return buffer
     }
 }
