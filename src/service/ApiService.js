229f31d12694b90481caa78cb493314bd78cd7aa
WillSantos08 <william.santana2@outlook.com>
2025-09-27 04:17:16 +0000

    commit

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -3,7 +3,7 @@ import { Routes } from "../router/Routes.js";
 export class ApiService extends Routes {
     constructor() {
         super()
-        this.baseURL = 'https://progressofit-backend.onrender.com/';
+        this.baseURL = 'https://progressofit-backend.onrender.com';
         this.headers = {
             'Content-Type': 'application/json',
         };
