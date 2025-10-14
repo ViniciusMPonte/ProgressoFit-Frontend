0254c859d111b31ad1206c0301d577d1b5fa0fb5
vinicius <vinicius.mponte@gmail.com>
2025-10-14 20:19:28 -0300

    refactor: criação do EditProfileComponent para centralização da lógica

--- src/view/PerfilView.js
+++ src/view/PerfilView.js
@@ -1,104 +1,30 @@
-import BaseView from "./BaseView.js";
-import { CopyrightComponent } from "./component/CopyrightComponent.js";
-import { AvatarComponent } from "./component/AvatarComponent.js";
+import BaseView from './BaseView.js'
+import { EditProfileComponent } from './component/profile/EditProfileComponent.js'
+import { CopyrightComponent } from './component/CopyrightComponent.js'
 
 export class PerfilView extends BaseView {
-
     constructor(dom) {
         super()
         this.dom = dom
     }
 
-    enableEdit() {
-        this.enableEditForm()
-        this.enableEditButtons()
+    renderEditProfileComponent(cbFunction) {
+        const targetTag = this.dom.getProfileContainer()
+        if (!targetTag) return
+
+        const component = new EditProfileComponent(targetTag)
+
+        if (cbFunction) {
+            component.componentService.setCallbackForm(cbFunction)
+        }
+
+        component.autoRender()
     }
 
-    disableEdit() {
-        this.disableEditForm()
-        this.disableEditButtons()
-    }
-
-    enableEditForm() {
-        const avatarOptContainer = this.dom.getAvatarOptions()
-        avatarOptContainer.classList.add('editing')
-
-        const inputs = [
-            this.dom.getNameInput(),
-            this.dom.getEmailInput(),
-            this.dom.getPasswordInput()
-        ];
-
-        inputs.forEach(input => {
-            if (!input) return
-
-            input.removeAttribute('readonly');
-            input.classList.remove('form-control-plaintext');
-            input.classList.add('form-control');
-
-        });
-    }
-
-    disableEditForm() {
-        const avatarOptContainer = this.dom.getAvatarOptions()
-        avatarOptContainer.classList.remove('editing')
-
-        const inputs = [
-            this.dom.getNameInput(),
-            this.dom.getEmailInput(),
-            this.dom.getPasswordInput()
-        ];
-
-        inputs.forEach(input => {
-            if (!input) return
-
-            input.setAttribute('readonly', true);
-            input.classList.remove('form-control');
-            input.classList.add('form-control-plaintext');
-
-        });
-    }
-
-    enableEditButtons() {
-        const editButton = this.dom.getEditButton();
-        const saveButton = this.dom.getSaveButton();
-        const cancelButton = this.dom.getCancelButton();
-
-        if (editButton) editButton.classList.add('d-none');
-        if (saveButton) saveButton.classList.remove('d-none');
-        if (cancelButton) cancelButton.classList.remove('d-none');
-    }
-
-    disableEditButtons() {
-        const editButton = this.dom.getEditButton();
-        const saveButton = this.dom.getSaveButton();
-        const cancelButton = this.dom.getCancelButton();
-
-        if (editButton) editButton.classList.remove('d-none');
-        if (saveButton) saveButton.classList.add('d-none');
-        if (cancelButton) cancelButton.classList.add('d-none');
-    }
-
-    swapSelected(allTags, selectedTag) {
-        allTags.forEach(opt => opt.classList.remove('selected'));
-        selectedTag.classList.add('selected');
-    }
-
-    selectAvatarOptByImgName(profileImgName) {
-        const selectedImg = document.querySelector(`.image-option[data-image="${profileImgName}"]`)
-        if (!selectedImg) return
-
-        const avatarOptionsContainer = this.dom.getAvatarOptions()
-        const imageOptions = [...avatarOptionsContainer.children]
-
-        this.swapSelected(imageOptions, selectedImg)
-    }
-
-    renderAvartarImgOptions() {
-        return new AvatarComponent().getAllAvatarImgOptions();
-    }
-
-    static renderFooter() {
-        return CopyrightComponent.get();
+    renderFooter() {
+        const footerTag = document.querySelector('#footer')
+        if (footerTag) {
+            footerTag.innerHTML = CopyrightComponent.get()
+        }
     }
 }
